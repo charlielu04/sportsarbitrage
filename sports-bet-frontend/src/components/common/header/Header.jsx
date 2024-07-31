82bd75b6ee39edd04f93d70fc1f5e5cb50b442c2
charlielu04 <charleslu04@gmail.com>
2024-07-30 22:06:17 -0400

    favicon and title

--- sports-bet-frontend/src/components/common/header/Header.jsx
+++ sports-bet-frontend/src/components/common/header/Header.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { Navbar } from '../../common';
-import './Header.css'
-import logo from './logo.png';
+import React from "react";
+import { Navbar } from "../../common";
+import "./Header.css";
+import logo from "./logo.png";
 
-function Header(){
-
-
-    return (
-        <section className="header">
-        <section className="header-top">
-          <section className="header-top__logo">
-            <a href="/">
-                <img src= {logo} alt = "Logo" className="logo"/>
-            </a>
-          </section>
-          <section className="header-top__navbar">
-            <section className="header-top__navigation">
-              <Navbar />
-            </section>
-            <hr className="header-top__seperator" />
+function Header() {
+  return (
+    <section className="header">
+      <section className="header-top">
+        <section className="header-top__logo">
+          <a href="/">
+            <img src={logo} alt="Logo" className="logo" />
+          </a>
+        </section>
+        <h1 className="arbedge"> Arbedge</h1>
+        <section className="header-top__navbar">
+          <section className="header-top__navigation">
+            <Navbar />
           </section>
         </section>
       </section>
-    );
-
+      <section>
+        <hr className="header-top__seperator" />
+      </section>
+    </section>
+  );
 }
 
 export default Header;
